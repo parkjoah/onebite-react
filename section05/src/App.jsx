ba592321abe313d6252cd4fbdf4eb3fec1defc4d
조아 <parkjoah@naver.com>
2025-02-26 17:03:08 +0900

    feat: counter bulb

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,38 +1,17 @@
 import { use } from "react";
 import "./App.css";
+import Bulb from "./components/Bulb";
+import Counter from "./components/Counter";
 // import Header from "./components/Header";
 // import Main from "./components/Main";
 // import Footer from "./components/Footer";
 // import Button from "./components/Button";
-import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [light, setLight] = useState("OFF");
-
   return (
     <>
-      <div>
-        <h1>{light}</h1>
-        <button
-          onClick={() => {
-            setLight(light === "ON" ? "OFF" : "ON");
-          }}
-        >
-          {light === "ON" ? "끄기" : "켜기"}
-        </button>
-      </div>
-      <div>
-        {" "}
-        <h1>{count}</h1>
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          +
-        </button>
-      </div>
+      <Bulb />
+      <Counter />
     </>
   );
 }
