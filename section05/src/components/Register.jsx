186e02fde29f57e1c5c457dedcb771a3040de47b
조아 <parkjoah@naver.com>
2025-02-26 18:57:50 +0900

    feat : useRef

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -1,7 +1,7 @@
-//간단한 회원가입 폼
+// 간단한 회원가입 폼
 // 1이름 2생년월일 3국적 4자기소개
 
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Register = () => {
   const [input, setInput] = useState({
@@ -11,7 +11,9 @@ const Register = () => {
     bio: "",
   });
 
-  console.log(input);
+  const refObj = useRef(0);
+  // console.log("레지스터 리렌더링");
+  const inputRef = useRef();
 
   const onChange = (e) => {
     setInput({
@@ -19,10 +21,26 @@ const Register = () => {
       [e.target.name]: e.target.value,
     });
   };
+  const onSubmit = () => {
+    if (input.name === "") {
+      inputRef.current.focus();
+    }
+  };
   return (
     <>
       <div>
+        <button
+          onClick={() => {
+            refObj.current++;
+            console.log(refObj.current);
+          }}
+        >
+          ref + 1
+        </button>
+      </div>
+      <div>
         <input
+          ref={inputRef}
           name="name"
           value={input.name}
           onChange={onChange}
@@ -48,6 +66,7 @@ const Register = () => {
       <div>
         <textarea name="bio" value={input.bio} onChange={onChange} />
       </div>
+      <button onClick={onSubmit}>제출</button>
     </>
   );
 };
