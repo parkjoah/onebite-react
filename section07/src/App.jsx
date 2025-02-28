9edc28cb886fae2031923a370c1971e8711c2d5d
조아 <parkjoah@naver.com>
2025-02-28 18:46:30 +0900

    feat: useEffect 사이드이펙트 제어!

--- section07/src/App.jsx
+++ section07/src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import Viewer from "./components/viewer";
 import Controller from "./components/Controller";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    console.log(`count: ${count} / input: ${input}`);
+  }, [count, input]);
 
   const onClickButton = (value) => {
     setCount(count + value);
@@ -13,6 +18,14 @@ function App() {
   return (
     <div className="App">
       <h1>Simple Counter</h1>
+      <section>
+        <input
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+      </section>
       <section>
         <Viewer count={count} />
       </section>
