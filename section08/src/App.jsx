176f8467dfbd3cef689fee9d5e408b11052d9000
조아 <parkjoah@naver.com>
2025-03-03 23:20:29 +0900

    feat: todo mockData

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,9 +1,32 @@
 import "./App.css";
+import { useState } from "react";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
 
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "커밋하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 0,
+    isDone: false,
+    content: "알바",
+    date: new Date().getTime(),
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(mockData);
   return (
     <>
       <div className="App">
