5f428aa104f2fc10bb5cb5c4e9c494313e07cbf9
조아 <parkjoah@naver.com>
2025-03-28 20:30:01 +0900

    feat: useCallback

--- section10/src/components/TodoItem.jsx
+++ section10/src/components/TodoItem.jsx
@@ -23,16 +23,18 @@ const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   );
 };
 
-// 고차 컴포넌트(HOC)
-export default memo(TodoItem, (prevProps, nextProps) => {
-  // 반환값에 따라, props가 바뀌었는지 안바뀌었는지를 판단!
-  // T -> props 바뀜 x -> 리렌더링 x
-  // F -> props 바뀜 o -> 리렌더링 o
+// // 고차 컴포넌트(HOC)
+// export default memo(TodoItem, (prevProps, nextProps) => {
+//   // 반환값에 따라, props가 바뀌었는지 안바뀌었는지를 판단!
+//   // T -> props 바뀜 x -> 리렌더링 x
+//   // F -> props 바뀜 o -> 리렌더링 o
 
-  if (prevProps.id !== nextProps.id) return false;
-  if (prevProps.isDone !== nextProps.isDone) return false;
-  if (prevProps.content !== nextProps.content) return false;
-  if (prevProps.date !== nextProps.date) return false;
+//   if (prevProps.id !== nextProps.id) return false;
+//   if (prevProps.isDone !== nextProps.isDone) return false;
+//   if (prevProps.content !== nextProps.content) return false;
+//   if (prevProps.date !== nextProps.date) return false;
 
-  return true;
-});
+//   return true;
+// });
+
+export default memo(TodoItem);
